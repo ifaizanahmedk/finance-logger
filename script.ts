fcfda99569d286a6716e23e5fa6a7c4a32696e30
Faizan Ahmed <fahmed@SUADEO.NET>
2026-01-06 15:03:29 +0400

    Typescript: Objects and Arrays

--- script.ts
+++ script.ts
@@ -1,13 +1,34 @@
-let character ='mario';
-let age = 30;
-let isBlackBelt = false;
 
-character = 'luigi';
-age = 40;
-isBlackBelt = true;
+//Arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-}
+names.push('toad');
+// names.push(3); // error
+// names[1] = 25; // error
+// names = 'hello'; // error
 
-console.log(circ(7.5));
+let numbers = [10, 20, 30];
+numbers.push(40);
+// numbers.push('shaun'); // error
+// numbers[1] = 'luigi'; // error
+// numbers = 'hello'; // error
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10); 
+mixed[0] = 3;
+
+// objects
+let ninja = {
+    name: 'yoshi',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ken';
+// ninja.age = '30'; // error
+// ninja.skills = ['fighting', 'sneaking']; // error
+// ninja = {}; // error
+
+
